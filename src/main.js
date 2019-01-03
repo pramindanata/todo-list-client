e2cb903a6e6bca380d373de8af5b406341552ff1
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2019-01-03 19:56:26 +0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './routers/index';
+import store from './stores/index';
+
+import './plugins/vuetify';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
