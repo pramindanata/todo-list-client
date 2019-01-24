999081cc0c0a0d27bcace5b506a9ace0073349ff
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2019-01-24 23:04:10 +0800

    add alert component

---
+++ src/components/AlertDanger.vue
@@ -0,0 +1,33 @@
+<template>
+  <base-alert
+    type="error"
+    :messages="messages"
+    :alert="alert"
+    @set-alert="setAlert($event)"
+  ></base-alert>
+</template>
+
+<script>
+export default {
+  props: {
+    messages: {
+      default: () => [],
+      required: true,
+      type: Array,
+    },
+    alert: {
+      default: false,
+      required: true,
+      type: Boolean,
+    },
+  },
+  components: {
+    BaseAlert: () => import('./base/Alert.vue'),
+  },
+  methods: {
+    setAlert(value) {
+      this.$emit('set-alert', value);
+    },
+  },
+};
+</script>
---
+++ src/components/base/Alert.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mb-3">
+    <v-alert
+      dismissible
+      transition="fade-transition"
+      v-model="alertModel"
+      :type="type"
+    >
+      <ul v-if="messages.length > 1">
+        <li v-for="(message, i) in messages" :key="i">{{ message }}</li>
+      </ul>
+
+      <span v-else>{{ messages[0] }}</span>
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      required: true,
+      type: String,
+    },
+    messages: {
+      default: () => [],
+      required: true,
+      type: Array,
+    },
+    alert: {
+      default: false,
+      required: true,
+      type: Boolean,
+    },
+  },
+  computed: {
+    alertModel: {
+      get() {
+        return this.alert;
+      },
+      set(value) {
+        this.$emit('set-alert', value);
+      },
+    },
+  },
+};
+</script>
