01067962e79e0f4c8c2e91640a85303aed46351d
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2019-01-25 19:42:47 +0800

    add alerts provider component

---
+++ src/components/Alerts.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <base-alert
+      type="error"
+      :alert="alert.error.status"
+      :messages="alert.error.messages"
+      @set-alert-error="setAlert($event)"
+    ></base-alert>
+
+    <base-alert
+      type="success"
+      :alert="alert.success.status"
+      :messages="alert.success.messages"
+      @set-alert-success="setAlert($event)"
+    ></base-alert>
+
+    <base-alert
+      type="warning"
+      :alert="alert.warning.status"
+      :messages="alert.warning.messages"
+      @set-alert-warning="setAlert($event)"
+    ></base-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    alert: {
+      required: true,
+      type: Object,
+    },
+  },
+  components: {
+    BaseAlert: () => import('./base/Alert.vue'),
+  },
+  methods: {
+    setAlert(value) {
+      this.$emit('set-alert', value);
+    },
+  },
+};
+</script>
